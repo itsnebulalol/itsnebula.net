c3611c2877491f518135314403cbe50f0c3edb8b
Nebula <me@itsnebula.net>
2023-04-10 11:15:00 -0400

    chore: disable SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     config: {
       applyBaseStyles: false
     }
-  }), react()],
-  output: "server",
-  adapter: cloudflare()
+  }), react()]
 });
