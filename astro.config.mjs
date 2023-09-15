2ee8c91d3d977c94367093ca476ac35b75166d98
Nebula <me@itsnebula.net>
2023-09-15 11:28:46 -0400

    chore: revert switch to bun

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import million from 'million/compiler';
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,9 +12,5 @@ export default defineConfig({
   }), react()],
   vite: {
     plugins: [million.vite({ mode: 'react', server: true, auto: true })],
-  },
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  }
 })
